9f5f0a4931be6a591aee86a03c54348f8bbb223c
Maxwell Sieglinger <maxwellms@msn.com>
2016-11-27 18:50:18 -0600

    :)

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,9 +4,10 @@ int age1;
 
 int main( void )
 {
-printf("Hello, type your age: ");
+// Receives your age
+    printf("Hello, type your age: ");
     scanf(" %d", &age1);
-
-    printf("you will be %d in 5 years", age1+5);
+// Prints your age 5 years into the future
+    printf("You will be %d in 5 years", age1+5);
     return 0;
 }
