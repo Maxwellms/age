bbe7474d9ba4dd92f8ed377ad42daaeb4059fd73
Maxwell <maxwellms@msn.com>
2016-11-27 18:57:38 -0600

    Update age.c

--- age.c
+++ age.c
@@ -4,11 +4,11 @@ int age1;
 
 int main( void )
 {
-// Receives your age
+    // Receives your age
     printf("Hello, type your age: ");
     scanf(" %d", &age1);
     
-// Prints your age 5 years into the future
+    // Prints your age 5 years into the future
     printf("You will be %d in 5 years", age1+5);
     return 0;
 }
